da963c7cd3bb6cb5d5ae4fea618b9de30d860bc4
marrrkitos <immakroteves@gmail.com>
2024-07-07 12:18:02 -0300

    Arreglos SEO

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,8 +6,8 @@ const Card = ({ parrafo, alinear, urlGitHub, urlNetlify, urlImg }) => {
             <img src={urlImg} alt="imagen" className='imgCard text-neutral-200 font-light rounded-lg border border-yellow-500' />
             <p className="parraphCard text-yellow-500 text-lg lg:text-xl drop-shadow-[1px_1px_1px_rgba(0,0,0,1)]">{parrafo}</p>
             <div className="linksCard text-neutral-200 flex justify-around w-full p-2 items-center">
-                <a href={urlNetlify} className="linkCard" target='_blank'><img src="/link-45deg.svg" alt="linkIcon" className='linkIcon invert opacity-70 lg:hover:opacity-90 w-8 lg:hover:scale-105 transition-all'/></a>
-                <a href={urlGitHub} className="linkCard" target='_blank'><img src="/github.svg" alt="linkedinLogo" className='linkedinLogo invert opacity-70 lg:hover:opacity-90 w-6 lg:hover:scale-105 transition-all'/></a>
+                <a href={urlNetlify} className="linkCard" target='_blank'><img src="/link-45deg.webp" alt="linkIcon" className='linkIcon invert opacity-70 lg:hover:opacity-90 w-8 lg:hover:scale-105 transition-all'/></a>
+                <a href={urlGitHub} className="linkCard" target='_blank'><img src="/github.webp" alt="linkedinLogo" className='linkedinLogo invert opacity-70 lg:hover:opacity-90 w-6 lg:hover:scale-105 transition-all'/></a>
             </div>
         </div>
     )
