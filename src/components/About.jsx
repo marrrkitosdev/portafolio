a9d9cb749112e003d12b6854beb6cf0e2d84632d
marrrkitos <immakroteves@gmail.com>
2024-07-07 22:00:45 -0300

    Img tamaño fijo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                 <img src="/portal.webp" alt="" rel='preload' className='w-16 h-16 absolute left-0' id='invertirPortal' />
                 <img src='/allay.webp' alt="allay" rel='preload' className='allayGif w-16 h-16 rounded-full transition-all pb-2 animate-[volar3_10s_linear_infinite] sm:animate-[volar_10s_linear_infinite] md:animate-[volar2_12s_linear_infinite]' />
                 <div className="aboutMe grid grid-cols-1 justify-items-center border border-yellow-500 p-2 rounded-lg gap-5 bg-neutral-200 bg-opacity-30">
-                    <img src="/miniyo.webp" rel='preload' alt="miniyo" className='w-64 h-fit rounded-lg border border-yellow-500' />
+                    <img src="/miniyo.webp" rel='preload' alt="miniyo" className='w-64 h-48 rounded-lg border border-yellow-500' />
                     <p className='w-full h-fit text-yellow-500 drop-shadow-[1px_1px_1px_rgba(0,0,0,1)] text-lg lg:text-xl text-center'>
                         {lang === 'ESP' ? 
                         '¡Hola! Soy Marcos, tengo 19 años y soy desarrollador Frontend. Me considero una persona autodidacta, detallista, objetiva y adaptable. Actualmente estoy profundizando en mis conocimientos de React, CSS y TailwindCSS. A su vez estoy experimentando un poco con Astro.' : 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({ parrafo, alinear, urlGitHub, urlNetlify, urlImg }) => {
     return (
         <div className={`card rounded-lg bg-neutral-200 bg-opacity-30 flex flex-col items-center p-2 gap-4 border border-yellow-500 w-80 justify-between text-center ${alinear}`}>
-            <img src={urlImg} alt="imagen" className='imgCard text-neutral-200 font-light rounded-lg border border-yellow-500' />
+            <img src={urlImg} alt="overview" className='imgCard w-64 h-36 text-neutral-200 font-light rounded-lg border border-yellow-500' />
             <p className="parraphCard text-yellow-500 text-lg lg:text-xl drop-shadow-[1px_1px_1px_rgba(0,0,0,1)] p-2">{parrafo}</p>
             <div className="linksCard text-neutral-200 flex justify-around w-full p-2 items-center">
                 <a href={urlNetlify} className="linkCard" target='_blank'><img src="/link-45deg.webp" alt="linkIcon" className='linkIcon invert opacity-70 lg:hover:opacity-90 w-8 lg:hover:scale-105 transition-all'/></a>
